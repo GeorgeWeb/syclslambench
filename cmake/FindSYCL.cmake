56da61e1d502f0781bc0e518283455440995a6cc
Andrew Gozillon <andrew.gozillon@yahoo.com>
2018-12-13 18:01:52 -0800

    Forgot to hit save after some changes...woops

--- cmake/FindSYCL.cmake
+++ cmake/FindSYCL.cmake
@@ -3,7 +3,7 @@ find_path(SYCL_CC_PATH syclcc)  # Ensure syclcc exe script is in PATH
 
 get_filename_component(barename "$ENV{CXX}" NAME)
 
-if    (SYCL_CC_PATH AND ${barename} STREQUAL "syclcc")
+if    (SYCL_CC_PATH AND "${barename}" STREQUAL "syclcc")
   set (SYCL_FOUND TRUE)
 endif ()
 
--- cmake/FindTriSYCLCC.cmake
+++ cmake/FindTriSYCLCC.cmake
@@ -3,7 +3,7 @@ find_path(TRISYCL_CC_PATH trisyclcc)  # Ensure trisyclcc exe script is in PATH
 
 get_filename_component(barename "$ENV{CXX}" NAME)
 
-if    (TRISYCL_CC_PATH AND ${barename} STREQUAL "trisyclcc")
+if    (TRISYCL_CC_PATH AND "${barename}" STREQUAL "trisyclcc")
   set (TRISYCL_FOUND TRUE)
 endif ()
 
