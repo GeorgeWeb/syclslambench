73a40b95f32a390459419c1d80359cb6fa54a82e
Andrew Gozillon <andrew.gozillon@yahoo.com>
2019-01-23 15:33:40 -0800

    Modification of reduceKernel and Minor CMakeList modificaitons
    Now use a plain SYCL parallel_workgroup for the reduce kernel rather 
    than a DAGR parallel_for, this is to avoid explicit use of barriers 
    which triSYCL's OpenMP implementation doesn't handle well (very slow). 
    Doesn't seem to affect ComputeCpp performance.
    
    Minor modifications to improve CMakeLists.txt. 
    
    The first is adding -fopenmp which was missing and causing the -cpp 
    implementation to run a lot slower than it should, at least on my 
    machine at Xilinx. This appears to be missing in the actual SLAMBench 
    repository as well, so maybe worth opening up an issue or merge request 
    related to it.
    
    Second is to change add_definitions -> target_compile_definitions, which 
    means the definitions will only be present when compiling for the 
    specific target. This means that depending on the syclcc script in use 
    more SLAMBench modules are compileable...rather than having to swap 
    between scripts. 
    
    Full integration would be ideal, perhaps it's possible to swap to using 
    the scripts when compiling for the sycl module...?

--- kfusion/src/sycl/kernels.cpp
+++ kfusion/src/sycl/kernels.cpp
@@ -5,6 +5,7 @@
 
  */
 #ifdef TRISYCL
+#define TRISYCL_NO_BARRIER
 #include <CL/sycl.hpp>
 #else
 #include <SYCL/sycl.hpp>
@@ -329,7 +330,8 @@ static void k(item<2> ix, T *normal, const T *verte_)
 
 }; // struct
 
-struct reduceKernel {
+
+struct old_reduceKernel {
 
 template <typename T, typename U, typename V>
 static void k(nd_item<1> ix, T *out, const U *J_,
@@ -891,20 +893,112 @@ bool Kfusion::tracking(float4 k, float icp_threshold,
         dagr::ro(*ocl_vertex),             dagr::ro(*ocl_normal),
         pose,projectReference,dist_threshold,normal_threshold);
 
-      const    range<1> nitems{size_of_group * number_of_groups};
-      const nd_range<1> ndr{nd_range<1>(nitems, range<1>{size_of_group})};
-
 #ifndef DEBUG_OFF
       printArraySlice(reduceOutputBuffer, number_of_groups * 32);
       printBufferSlice(ocl_trackingResult, (uint)computationSize.x() * computationSize.y());
       std::cout << std::endl;
 #endif
 
-      dagr::run<reduceKernel,0>(q,ndr,
-        dagr::wo(buffer<float,1>(reduceOutputBuffer,
-                                 range<1>{32 * number_of_groups})),
-        dagr::ro(*ocl_trackingResult), computationSize, localimagesize,
-        dagr::lo<float>(size_of_group * 32));
+{
+  auto rob = cl::sycl::buffer<float,1>(reduceOutputBuffer, range<1>{32 * number_of_groups});
+
+  q.submit([&](cl::sycl::handler &cgh) {
+    auto JSize = computationSize;
+    auto size = localimagesize;
+
+    auto out = rob.get_access<cl::sycl::access::mode::write>(cgh);
+    auto J_ = ocl_trackingResult->get_access<cl::sycl::access::mode::read>(cgh);
+
+     cl::sycl::accessor<float, 1, cl::sycl::access::mode::read_write,
+      cl::sycl::access::target::local> S(cl::sycl::range<1>{size_of_group * 32}, cgh);
+
+    // Why does ours accept an NDR but ComputeCpp does not?
+    cgh.parallel_for_work_group<class ReduceKernel_lambda>(
+        range<1>{number_of_groups}, range<1>{size_of_group},
+        [=](cl::sycl::group<1> g) {
+
+          g.parallel_for_work_item([=](cl::sycl::h_item<1> tile) {
+            uint blockIdx  = g.get_id(0);
+            uint blockDim  = g.get_local_range(0);
+            uint threadIdx = tile.get_local_id(0);
+            uint gridDim = g.get_group_range(0);
+
+            const uint sline = threadIdx;
+
+            float         sums[32];
+            float *jtj  = sums + 7;
+            float *info = sums + 28;
+
+            for (uint i = 0; i < 32; ++i)
+              sums[i] = 0.0f;
+
+            for (uint y = blockIdx; y < size.y(); y += gridDim) {
+              for (uint x = sline; x < size.x(); x += blockDim) {
+                const TrackData row = J_[x + y * JSize.x()];
+
+                if (row.result < 1) {
+                  info[1] += row.result == -4 ? 1 : 0;
+                  info[2] += row.result == -5 ? 1 : 0;
+                  info[3] += row.result > -4 ? 1 : 0;
+                  continue;
+                }
+                sums[0] += row.error * row.error;
+
+                // JTe part
+                for (int i = 0; i < 6; ++i)
+                  sums[i + 1] += row.error * row.J[i];
+
+                jtj[0] += row.J[0] * row.J[0];
+                jtj[1] += row.J[0] * row.J[1];
+                jtj[2] += row.J[0] * row.J[2];
+                jtj[3] += row.J[0] * row.J[3];
+                jtj[4] += row.J[0] * row.J[4];
+                jtj[5] += row.J[0] * row.J[5];
+
+                jtj[6] += row.J[1] * row.J[1];
+                jtj[7] += row.J[1] * row.J[2];
+                jtj[8] += row.J[1] * row.J[3];
+                jtj[9] += row.J[1] * row.J[4];
+                jtj[10] += row.J[1] * row.J[5];
+
+                jtj[11] += row.J[2] * row.J[2];
+                jtj[12] += row.J[2] * row.J[3];
+                jtj[13] += row.J[2] * row.J[4];
+                jtj[14] += row.J[2] * row.J[5];
+
+                jtj[15] += row.J[3] * row.J[3];
+                jtj[16] += row.J[3] * row.J[4];
+                jtj[17] += row.J[3] * row.J[5];
+
+                jtj[18] += row.J[4] * row.J[4];
+                jtj[19] += row.J[4] * row.J[5];
+
+                jtj[20] += row.J[5] * row.J[5];
+                // extra info here
+                info[0] += 1;
+              }
+            }
+
+            // copy over to shared memory
+            for (int i = 0; i < 32; ++i)
+              S[sline * 32 + i] = sums[i];
+          });
+
+          g.parallel_for_work_item([=](cl::sycl::h_item<1> tile) {
+            uint blockIdx  = g.get_id(0);
+            uint blockDim  = g.get_local_range(0);
+            uint threadIdx = tile.get_local_id(0);
+            const uint sline = threadIdx;
+
+            if (sline < 32) {
+              for (unsigned i = 1; i < blockDim; ++i)
+                S[sline] += S[i * 32 + sline];
+                out[sline + blockIdx * 32] = S[sline];
+            }
+          });
+        });
+  });
+}
 
 #ifndef DEBUG_OFF
         printArraySlice(reduceOutputBuffer, number_of_groups * 32);
